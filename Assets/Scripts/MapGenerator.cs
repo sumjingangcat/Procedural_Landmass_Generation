e30d2f4c1476e4a2435f3cbf72d4aeede5959b4a
sumjingangcat <oj47191@gmail.com>
2023-03-23 11:45:44 +0900

    [Chore] clean up

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,38 @@ public class MapGenerator : MonoBehaviour {
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
+    
+    public int seed;
+    
+    public int octaves;
+    
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public Vector2 offset;
+
+    public bool autoUpdate;
 
     public void GenerateMap() {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, seed, octaves, persistance, lacunarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate() {
+        if (mapWidth < 1) {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1) {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1) {
+            lacunarity = 1;
+        }
+        if (octaves < 0) {
+            octaves = 0;
+        }
     }
 }
